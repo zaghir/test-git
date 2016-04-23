f16667edc677a66394f0fbb6e6c66cd9606a46e4
yzaghir <yzaghir@PAR-N-PW07-4807.BETD.FR>
2016-04-23 18:12:32 +0200

    2éme modification

--- Test-GIT/src/main/java/Class1.java
+++ Test-GIT/src/main/java/Class1.java
@@ -3,6 +3,10 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("-----zzzzzz");
+		
+		System.out.println("Message ");
+		
+		System.out.println("message 2");
 
 	}
 
